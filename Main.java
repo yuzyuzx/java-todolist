6245923e0c9d1db519c845d0140ff9245436ba95
天野 裕司 <yamano@gway.co.jp>
2024-08-27 15:27:13 +0900

    commit

--- Main.java
+++ Main.java
@@ -1,8 +1,56 @@
-package todo;
+import java.util.Scanner;
+
+enum OPE {
+  CREATE
+}
 
 public class Main {
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String input = "";
     
+    boolean isLoop = true;
+    if(input.equalsIgnoreCase("exit")) {
+      isLoop = false;
+    }
+
+
+    while(isLoop) {
+      System.out.println("操作番号を選択してください");
+      System.out.println("アプリを終了するにはqを入力してください");
+      
+      input = sc.nextLine();
+      
+      switch(input) {
+        case "1":
+          System.out.println("一覧表示");
+          break;
+          
+        case "2":
+          System.out.println("新規作成");
+          break;
+        
+        case "3":
+          System.out.println("削除");
+          break;
+        
+        default:
+          System.out.println("指定した操作は存在しません");
+          break;
+      }
+      
+
+      if(input.equalsIgnoreCase("exit")) {
+        isLoop = false;
+      }
+
+      
+      System.out.println("\n");
+    }
+    
+    System.out.println("アプリを終了します\n");
+    
+    sc.close();
   }
   
 }
